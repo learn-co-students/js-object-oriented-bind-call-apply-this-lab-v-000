2b8bd42a2ac7c90886e57a8fff79dff1ca085ea5
Lucas <lqf2b8@gmail.com>
2017-10-05 17:00:53 -0500

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+function justInvoke(fn) {
+    return fn()
+}
+
+function setThisWithCall(fn, thisValue) {
+    return fn.call(thisValue)
+}
+
+function setThisWithApply(fn, thisValue, args) {
+    return fn.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+    const newFunction = functionToBeCopied.bind(thisValue);
+    return newFunction;
+}
