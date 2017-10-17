e27254f08cfaf3e9947b6dd144f182b3fa592f8b
Tyler <tbuchhei@alumni.nd.edu>
2017-10-17 10:52:32 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+const justInvoke = (fn) => {
+  return fn();
+}
+
+const setThisWithCall = (fn, thisValue, arg) => {
+  return fn.call(thisValue, arg);
+}
+
+const setThisWithApply = (fn, thisValue, args) => {
+  return fn.apply(thisValue, args);
+}
+
+const returnNewFunctionOf = (functionToBeCopied, thisValue) => {
+  return functionToBeCopied.bind(thisValue);
+}
