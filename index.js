12b46d03295829b99d005b8750f7cb095afa2f06
github username <github email address>
2017-12-23 09:28:41 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const justInvoke = function(fn) {
+  return fn();
+}
+
+const setThisWithCall = function(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+const setThisWithApply = function(fn, thisValue, args) {
+  return fn.apply(thisValue, [...args]);
+}
+
+const returnNewFunctionOf = function(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
