ee2a2a12c1005c32e88cdb15079c1b21b8378038
Ashley Muller <serenehopes@gmail.com>
2018-02-22 03:41:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, [...args])
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  newFunction = functionToBeCopied.bind(thisValue)
+  return newFunction
+}
+
+function justInvoke(fn){
+  return fn();
+}
