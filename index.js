9567eea1ebd8ff243610d93c5c06491a4935c665
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-06 20:33:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function justInvoke(fn){
+   thisValue = function fn() {
+     return this
+   }
+   return fn()
+}
+
+function setThisWithCall(fn, value, arg) {
+  return fn.call(value, arg)
+}
+
+function setThisWithApply(fn,value,args) {
+  return fn.apply(value,args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue)
+}
