4dfc31ee679ada48a78ac7417becb5a8e5b88ad6
Sam Ojling <s.ojling@gmail.com>
2018-03-20 11:08:36 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, [age, hairColor]) {
+  return fn.apply(thisValue, [age, hairColor]);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
+
+console.log(returnNewFunctionOf);
