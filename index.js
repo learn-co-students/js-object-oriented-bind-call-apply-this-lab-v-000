f0adce38441ed0bdf2fbe8d036a76415c2041fd2
remclay <remmy.lauren@gmail.com>
2018-05-11 16:50:08 +1000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function justInvoke(fn) {
+  fn();
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(fn, thisValue) {
+  newFunction = fn.bind(thisValue);
+  return newFunction;
+}
