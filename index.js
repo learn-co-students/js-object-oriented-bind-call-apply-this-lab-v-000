1442aff4ed4a15ff6960d258d5e47043bee67c40
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-30 19:25:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, value, arg){
+  return fn.call(value, arg)
+}
+
+function setThisWithApply(fn, value, args){
+  return fn.apply(value, args)
+}
+
+function returnNewFunctionOf(fn, value){
+  return newFn = fn.bind(value)
+}
