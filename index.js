aad074411e8669f28600d7e9f970df41a84c8119
Emily Jirles <emily.jirles@gmail.com>
2018-07-13 16:05:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+};
+
+function setThisWithCall(fn, obj, arg){
+//  calls and returns the function as a callback, and assigns this to be bob while passing an argument to the invoked function
+  return fn.call(obj, arg);
+};
+
+function setThisWithApply(fn, obj, [age, hairColor]){
+  return fn.apply(obj, [age, hairColor]);
+};
+
+function returnNewFunctionOf(fn, arg) {
+  return fn.bind(arg);
+};
