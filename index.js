fe8d1a056d3ae2cb7b0b72f7c95a0dc2dc54dfe1
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 17:35:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Your code here
+//ƒ (){ return {thisValue: this, arguments: Array.from(arguments)} }
+function justInvoke(fn) {
+  return fn();
+};
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+};
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const newFunct = functionToBeCopied.bind(thisValue);
+  return newFunct;
+}
