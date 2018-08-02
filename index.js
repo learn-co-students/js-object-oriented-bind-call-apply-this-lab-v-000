fca0adaa8bac09b64406d51b5841e6eedbf8cbac
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 18:44:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//Your code here
+function justInvoke(callback){
+    return callback.call(window)
+}
+function setThisWithCall(callback, scope, value){
+  return callback.call(scope, value)
+}
+function setThisWithApply(callback, scope, args){
+  return callback.apply(scope, args)
+}
+function returnNewFunctionOf(callback, fred){
+  return function(){ return callback.apply(fred) }.bind(fred)
+}
