e48b5d5b5ecb180364eb0c3bd1378267441cb5b6
Araba <araba.taylor20@gmail.com>
2018-08-03 15:33:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, arg, age){
+  return fn.call(arg,age);
+}
+
+function setThisWithApply(fn, arg, traits){
+  return fn.apply(arg, traits);
+}
+
+function returnNewFunctionOf(fn, thisValue){
+  return fn.bind(thisValue);
+}
