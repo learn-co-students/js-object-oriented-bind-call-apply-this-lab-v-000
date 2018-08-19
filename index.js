b050ba79199169256617cef2741807e8003a4d8b
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-19 15:12:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Your code here
+const justInvoke = function(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg)
+}
+
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
