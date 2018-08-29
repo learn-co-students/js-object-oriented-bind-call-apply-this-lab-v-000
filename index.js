f5636a6d64df8800d9c216876eada1c3dbae6a7b
Katrina Brinson <kbrinso@gmail.com>
2018-08-29 20:00:15 +0000

    Done.

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ function setThisWithApply(cb, name, ary){
   return cb.apply(name,ary);
 }
 
-function returnNewFunctionOf(toCopy){
-  if (arguments.length > 0) {
-        const customers = Array.prototype.slice.call(arguments);
-
-        last = customers.pop();
-
-    console.log(`${this.name} for ${customers.join(', ')} and ${last}. Enjoy!`);
-    } else {
-        console.log(`${this.name}. Order up!`);
-    }
+function returnNewFunctionOf(toCopy, arg){
+  // if (arguments.length > 0) {
+  //       const customers = Array.prototype.slice.call(arguments);
+  //
+  //       last = customers.pop();
+  //
+  //   console.log(`${this.name} for ${customers.join(', ')} and ${last}. Enjoy!`);
+  //   } else {
+  //       console.log(`${this.name}. Order up!`);
+  //   }
+  return toCopy.bind(arg);
 }
