026c8484dad2f97fcd4670de1b4eafacab43607b
Katrina Brinson <kbrinso@gmail.com>
2018-08-29 19:28:13 +0000

    Stuck on returnNewFunction

---
+++ index.js
@@ -0,0 +1,23 @@
+function justInvoke(input){
+  return input();
+}
+
+function setThisWithCall(cb, name, age){
+  return cb.call(name, age);
+}
+
+function setThisWithApply(cb, name, ary){
+  return cb.apply(name,ary);
+}
+
+function returnNewFunctionOf(toCopy){
+  if (arguments.length > 0) {
+        const customers = Array.prototype.slice.call(arguments);
+
+        last = customers.pop();
+
+    console.log(`${this.name} for ${customers.join(', ')} and ${last}. Enjoy!`);
+    } else {
+        console.log(`${this.name}. Order up!`);
+    }
+}
