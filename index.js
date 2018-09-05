a85b21b265d2c0f6db6fc0927e823faa9cbfc358
github username <github email address>
2018-09-05 11:57:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+
+//calls and returns the function as a callback
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue,arg);
+}
+
+function setThisWithApply(fn, thisValue, arg){
+  return fn.apply(thisValue,arg);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
