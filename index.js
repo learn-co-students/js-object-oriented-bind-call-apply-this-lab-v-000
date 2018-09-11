4007eaf0e5e7760cbe5f46cd5387b303568f3407
Kkelly22 <kk504408@gmail.com>
2018-09-10 21:21:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Your code here
+
+function justInvoke (cb) {
+	return cb();
+};
+
+
+function setThisWithCall (cb, person, age) {
+	return cb.call(person, age);
+};
+
+
+function setThisWithApply (cb, person, age) {
+	return cb.apply(person, age);
+};
+
+
+function returnNewFunctionOf (cb, person, age) {
+	newFunction = cb.bind(person, age);
+	return newFunction;
+};
