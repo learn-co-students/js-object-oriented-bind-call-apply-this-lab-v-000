12119747349ade2c452ab20efd3ffb373a44c1d6
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-14 11:15:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Your code here
+
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn, obj, age) {
+  return fn.call(obj, age)
+}
+
+function setThisWithApply(fn, obj, args) {
+  return fn.apply(obj, [...args])
+}
+
+function returnNewFunctionOf(fn, obj) {
+  let newFunction = fn.bind(obj)
+  return newFunction
+}
