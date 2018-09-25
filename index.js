0abe822dc0cc27bbc59fd0cbc6913a5626046c9f
NeedsAUsername <kevinchi118@gmail.com>
2018-09-24 23:11:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Your code here
+function justInvoke(func) {
+  return func();
+}
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+}
+function setThisWithApply(fn, thisValue, arg) {
+  return fn.apply(thisValue, arg);
+}
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
