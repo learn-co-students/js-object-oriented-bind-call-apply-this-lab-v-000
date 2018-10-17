71dc769a66fb7e8bbd0c312039fd21c4578ca23a
theresamorelli <theresa.l.morelli@gmail.com>
2018-10-16 23:10:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, person, age) {
+  return fn.call(person, age);
+}
+
+function setThisWithApply(fn, person, array) {
+  return fn.apply(person, array);
+}
+
+function returnNewFunctionOf(fn, person) {
+  return fn.bind(person);
+}
