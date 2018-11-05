22e1b0bfb14e3c3b97b14c0c6f0aeb8fd6846a8b
pracniscate <lorena.giddens@outlook.com>
2018-11-05 15:03:21 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//Your code here
+// invokes the function passed through
+function justInvoke(fn) {
+    return fn();
+};
+
+// invokes the function passed through, & uses call() method to return the function's value
+function setThisWithCall(fn, thisValue, arg) {
+    return fn.call(thisValue, arg);
+};
+
+// invokes the function passed through, & change the value of THIS
+function setThisWithApply(fn, thisValue, arg) {
+    return fn.apply(thisValue, arg);
+};
+
+// returns a copy of the function passed through, & sets THIS value of the copy
+function returnNewFunctionOf(fn, thisValue) {
+    const copy = fn.bind(thisValue);
+    return copy;
+};
