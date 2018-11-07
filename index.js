86bac65a5a5969fafc0604f611163e894c3f6aa7
anikonchuk <anikonch@gmail.com>
2018-11-07 13:48:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+};
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+};
+
+function setThisWithApply(fn, thisValue, [age, hairColor]) {
+  return fn.apply(thisValue, [age, hairColor]);
+};
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+};
