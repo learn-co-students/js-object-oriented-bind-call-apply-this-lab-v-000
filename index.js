53e45fdc2092a25328a096db595d9b60a8334142
Anu Khambete <anukhambete@gmail.com>
2018-11-20 01:53:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke (fn) {
+  return fn();
+}
+
+function setThisWithCall (fn, name, args) {
+   return fn.call(name, args);
+}
+
+function setThisWithApply (fn, name, args) {
+  return fn.apply(name, args);
+}
+
+function returnNewFunctionOf (fn, name) {
+  return fn.bind(name);
+}
