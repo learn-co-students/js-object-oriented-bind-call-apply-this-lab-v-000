4a67ee09a9b5c4d551e8b1ebcb751ec1cb207728
alcesalces11331 <rollin.metzger@gmail.com>
2018-11-27 17:24:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+
+function justInvoke(cb) {
+    return cb();
+}
+
+function setThisWithCall(cb, name, number) {
+    return cb.call(name, number);
+}
+
+function setThisWithApply(cb, name, array) {
+    return cb.apply(name, array);
+}
+
+function returnNewFunctionOf(cb, name) {
+    return cb.bind(name);
+}
