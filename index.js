2792c03cd3351c764a2ed8bf653843e342458f67
Lucy Wall <lhwall@gmail.com>
2018-11-29 19:59:48 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ function justInvoke(passedFunction){
   return passedFunction()
 }
 
-function setThisWithCall(passedFunction){
-  return passedFunction()
+function setThisWithCall(passedFunction, thisValue, arg){
+  return passedFunction.call(thisValue, arg)
+}
+
+function setThisWithApply(passedFunction, thisValue, args){
+  return passedFunction.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const newFunction = functionToBeCopied.bind(thisValue)
+  return newFunction
 }
