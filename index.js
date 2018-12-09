5248fd8d2319217ec9401cf74f2f6695daf051ae
Jennifer Pazos <jmp521@gmail.com>
2018-12-09 00:24:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+
+justInvoke = (fN) => {
+  return fN()
+}
+
+setThisWithCall = (fN, thisValue, arg) => {
+  return fN.call(thisValue, arg)
+}
+
+setThisWithApply = (fN, thisValue, args) => {
+  return fN.apply(thisValue, args)
+}
+
+returnNewFunctionOf = (fN, thisValue) => {
+  var copy = fN.bind(thisValue)
+  return copy
+
+}
