75dcae3146aa01ca445cd228399383d2eb20c169
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-12-12 11:33:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, value, arg) {
+  return fn.call(value, arg);
+}
+
+function setThisWithApply(fn, value, arg) {
+  return fn.apply(value, arg);
+}
+
+function returnNewFunctionOf(fn, value) {
+  return fn.bind(value);
+}
