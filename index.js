07ca7a879c84b2469a7433e71927aef12ec4d86c
Christopher P. Dundon <chris.dundon@gmail.com>
2018-12-15 15:39:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Your code here
+function justInvoke (fn) {
+	return fn();
+}
+
+function setThisWithCall (fn, obj, age) {
+	return fn.call(obj, age);
+}
+
+function setThisWithApply (fn, obj, args) {
+	return fn.apply(obj, args);
+}
+
+function returnNewFunctionOf (fn, obj, args) {
+	const newFn = function () {
+		return this;
+	}.bind(obj);
+
+	return newFn;
+}
