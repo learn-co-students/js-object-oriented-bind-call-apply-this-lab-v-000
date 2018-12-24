91e891f58c1a9374bd8071ac82b8675206ea7a6e
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-12-24 15:43:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//Your code here
+function justInvoke(fn){
+  return   fn();
+
+}
+
+
+function setThisWithCall(fn, arg1 , arg2){
+  return fn.call(arg1, arg2)
+}
+
+
+function setThisWithApply(fn, thisV, arr){
+  return fn.apply(thisV, arr)
+}
+
+function returnNewFunctionOf(fn, thisV){
+  return fn.bind(thisV)
+}
