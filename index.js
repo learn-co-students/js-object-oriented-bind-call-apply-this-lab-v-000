aa684ce51123046a042cdf6958327ebda70949e3
charlie  lee <ctlee9096@gmail.com>
2018-12-28 21:03:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 //Your code here
+function justInvoke(fn) {
+  debugger;
+  return fn();
+  console.log(fn())
+  debugger;
+}
+
+function setThisWithCall(fn, thisValue, args) {
+  debugger;
+  return fn.call(thisValue, args);
+  console.log(fn.call(thisValue, args));
+  debugger;
+}
+function setThisWithApply(fn, thisValue, args) {
+  debugger;
+  return fn.apply(thisValue, args);
+  debugger;
+}
+
+function returnNewFunctionOf(fn, object, argurments){
+  const newFunction = function () {
+    return this;
+    debugger;
+  }.bind(object);
+  debugger;
+  return newFunction;
+  debugger;
+}
+
+// function Product(name, price) {
+//   this.name = name;
+//   this.price = price;
+// }
+//
+// function Food(name, price) {
+//   Product.call(this, name, price);
+//   this.category = 'food';
+// }
+//
+// console.log(new Food('cheese', 5).name);
