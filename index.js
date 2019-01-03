16f8297df3b2ae92a9a7fc0085ef90abf175d7e9
Jenny Kam <jkam17@gmail.com>
2019-01-03 16:30:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fnc){
+  return fnc()
+}
+
+function setThisWithCall(fnc, x, n){
+  return fnc.call(x, n)
+}
+
+function setThisWithApply(fnc, obj, args){
+  return fnc.apply(obj, args)
+}
+
+function returnNewFunctionOf(fnc, x){
+  return fnc.bind(x)
+}
