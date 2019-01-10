78ca4f1441a0c569b6712d6f57c2a9c682cde6f7
epic0448 <samuelogundiran@Yahoo.com>
2019-01-10 00:32:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+
+function justInvoke(returnsThisAndArg) {
+   return returnsThisAndArg()
+}
+
+function setThisWithCall(returnsThisAndArg, name, age){
+ return returnsThisAndArg.call(name, age )
+}
+
+function setThisWithApply(returnsThisAndArg, name, age){
+    return returnsThisAndArg.apply(name,age)
+}
+
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+    const copy = functionToBeCopied.bind(thisValue);
+    return copy;
+  }
