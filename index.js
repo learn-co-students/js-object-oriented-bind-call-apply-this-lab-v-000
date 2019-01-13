eaa9eee4ded2946b63cd783d99acb1b7e7a47d25
kylek <27875726+kylekinnear@users.noreply.github.com>
2019-01-13 12:42:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(funct) {
+  return funct();
+}
+
+function setThisWithCall(funct, thisVar, arg) {
+  return funct.call(thisVar, arg);
+}
+
+function setThisWithApply(funct, thisVar, arg) {
+  return funct.apply(thisVar, arg);
+}
+
+function returnNewFunctionOf(funct, thisVar) {
+  const newFunct = funct.bind(thisVar);
+  return newFunct;
+}
