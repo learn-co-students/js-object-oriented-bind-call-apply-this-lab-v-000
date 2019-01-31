8de19bd7ec87f796a17826bcac9359c32f580e35
amitnyc83 <amit_nyc@live.com>
2019-01-31 16:08:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 //Your code here
+// The function simply invokes the function passed through to it.
+// It also returns the return value of the passed through function.
+
 function justInvoke(fn){
   return fn.call();
 }
 
+// The function again invokes the function
+// passed to it, but uses the `call` method to return the function's this value.
+
 function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg);
 }
+
+// invoke the function passed to
+// it, change the `this` value of that function passed to it. In addition, we ask
+// you to invoked the passed function with arguments
 
 
 function setThisWithApply(fn, thisValue, args){
   return fn.apply(thisValue, args);
 }
 
+// write a function that returns a copy of the function passed through,
+// but sets the `this` value of the function's copy.
 
 function returnNewFunctionOf(functionToBeCopied, thisValue){
   return functionToBeCopied.bind(thisValue);
