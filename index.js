0212f70c687d2198f4b733a4058a167b1904d581
ConorHamilton19 <conhamil19@gmail.com>
2019-02-05 16:01:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn){
+  return fn.call()
+}
+
+function setThisWithCall(fn, value, arg){
+  return fn.call(value, arg)
+}
+
+
+function setThisWithApply(fn, value, args){
+  return fn.apply(value, args)
+}
+
+function returnNewFunctionOf(copy, value){
+  return copy.bind(value)
+}
