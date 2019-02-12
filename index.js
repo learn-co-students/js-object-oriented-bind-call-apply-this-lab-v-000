60ddf03558fe04d3c7ca3c7d1480baca50879421
github username <github email address>
2019-02-12 13:14:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //Your code here
+
+function justInvoke(fn){
+  return fn()
+}
+
+function setThisWithCall(fn, thisValue, arg){
+//   Because a callback function is invoked inside another function,
+//   we can forward to it any arguments passed to the outer function. For example:
+// function greet (name, cb) {
+//  return cb(name);
+// }
+
+  return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, [20, 'black'])
+
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const copy = functionToBeCopied.bind(thisValue);
+ return copy;
+}
