88a71ee13116cfaf10b2ba66a7db0d1b3a766794
mstmari <michael.landrum.st.marie@gmail.com>
2019-04-16 13:07:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-//Your code here
 function justInvoke(fn) {
-  return fn();
+  return fn()
 }
 
 function setThisWithCall(fn, thisValue, arg) {
-
-  return fn.call(thisValue, arg);
+    return fn.call(thisValue, arg)
 }
 
-function setThisWithApply(fn, thisValue, arg) {
-    return fn.apply(thisValue, arg);
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
-  return copiedFunction = functionToBeCopied.bind(thisValue);
-
+  return newFunction = functionToBeCopied.bind(thisValue)
 }
