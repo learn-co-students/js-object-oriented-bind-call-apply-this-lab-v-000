18162b14531b8759bbc39fe0c9003cb5a96fc866
Taiwei Ko <mike25ko@gmail.com>
2019-05-14 11:22:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+     return fn()
+ }
+
+  function setThisWithCall(fn, thisValue, argument) {
+     return fn.call(thisValue,argument)
+ }
+
+  function setThisWithApply(fn,thisValue,arguments) {
+     return fn.apply(thisValue,arguments)
+ }
+
+  function returnNewFunctionOf(fn,thisValue) {
+     const newFn = fn.bind(thisValue);
+     return newFn
+ } 
