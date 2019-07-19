55a052d890df79c840c98fa01f5339e8ee9efe6b
Meredith Reece <mardebugger@gmail.com>
2019-07-19 01:57:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+function setThisWithCall(fn, val, arg) {
+  return fn.call(val, arg);
+}
+function setThisWithApply(fn, val, arg) {
+  return fn.apply(val, arg);
+}
+function returnNewFunctionOf(fn, val) {
+  return fn.bind(val);
+}
