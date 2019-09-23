cfe41552b3671e176ab20da15f859d9c5af94c5b
Nate Dietz <nsdietz@gmail.com>
2019-09-23 13:04:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+
+function justInvoke(cb) {
+	return cb();
+}
+
+function setThisWithCall(fn, obj, arg) {
+	return fn.call(obj, arg);
+}
+
+function setThisWithApply(fn, obj, arg) {
+	return fn.apply(obj, arg);
+}
+
+function returnNewFunctionOf(fn, obj) {
+	const describeObj = fn.bind(obj);
+
+	return describeObj;
+}
